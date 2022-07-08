b7fe5bf3f29abc66b68d807607dae22778f1d420
abheist <abhishek291093@gmail.com>
2022-07-08 14:22:12 -0300

    working frontend

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const apiId = 'g7rurhbmm2'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-
-export const authConfig = {
-    domain: 'abheist.us.auth0.com',
-    clientId: 'fySFw22XSW91XD4HD4nUcreM6SNdZfL0',
-    callbackUrl: 'http://localhost:3000/callback'
-}
